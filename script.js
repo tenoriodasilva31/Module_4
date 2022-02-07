aa4c82ee94ea8d0f25fb8bbdcda2b9cb10defa0d
Jonathan Tenório da Silva <92552622+tenoriodasilva31@users.noreply.github.com>
2022-02-07 11:00:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 (function () {
 
-  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim", "Michelle", "Renata"];
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
   
   for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
